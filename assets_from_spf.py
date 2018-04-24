7ace99da0b3cc900e0d6a6b679aa77326e99ab14
synster <you@example.com>
2018-04-24 16:56:52 +0530

    Updated code that checks for SPF mechanisms

--- assets_from_spf.py
+++ assets_from_spf.py
@@ -1,6 +1,13 @@
 from __future__ import print_function
 import sys
-import dns.resolver
+import re
+import json
+
+try:
+    import dns.resolver
+except ImportError:
+    logging.info("\033[1;31m[!] Failed to import dnspython module. Run 'pip install dnspython'\033[1;m")
+    sys.exit()
 
 import logging
 
@@ -8,6 +15,10 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(message)s"
     )
+
+__author__  = "Bharath(github.com/yamakira)"
+__version__ = "0.0.1"
+__purpose__ = '''Extract domains/netblocks for a SPF record'''
 
 logger = logging.getLogger('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -30,21 +41,14 @@ def get_spf_record(domain):
 def get_assets(spf_record):
     assets = []
     spf_values = spf_record.split(" ")
+    # List of all mechanisms as part of SPF standard
+    mechanisms = ['ip4:','ip6:','ptr:','include:','a:','include:','mx:','exists:']
     for item in spf_values:
-        if 'ip4:' in item:
-            assets.append(item.replace("ip4:",""))
-        if 'ip6:' in item:
-            assets.append(item.replace("ip6:",""))
-        if 'include:' in item:
-            assets.append(item.replace("include:",""))
-        if 'ptr:' in item:
-            assets.append(item.replace("ptr:",""))
-        if 'a:' in item:
-            assets.append(item.replace("a:",""))
-        if 'mx:' in item:
-            assets.append(item.replace("mx:",""))
-        if 'exists:' in item:
-            assets.append(item.replace("exists:",""))
+        # Check for SPF mechanisms in each part of SPF record
+        if any(mechanism in item for mechanism in mechanisms):
+            # Replace the mechanism's keyword to extract only the asset value
+            asset = re.sub(r'|'.join(map(re.escape, mechanisms)), '', item)
+            assets.append(asset)
     return assets
 
 def get_domain():
@@ -60,5 +64,6 @@ def main():
     assets = get_assets(spf_record)
     for asset in set(assets):
         print(asset)
+
 if __name__ == '__main__':
     main()
